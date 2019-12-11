ea8ba047021e46d5395c95e5e2b967b56499ede5
ime <986597535@qq.com>
2019-12-11 15:49:05 +0800

    init

---
+++ .history/src/transform_20191211101526.ts
@@ -0,0 +1,43 @@
+export function transform(data) {
+  
+}
+
+function toInterface (variable) {
+  var interfaceNames = ['IUnknown'];
+  var extra_interface = '';
+ 
+  var interface = generateInterface('IUnknown', variable, '');
+  return (interface + '\n/* 自动生成的 Interface */\n' + extra_interface).trim();
+};
+
+function formatKey (key) {
+  if (!/^[a-z][a-z\d]*$/i.test(key))
+    return JSON.stringify(key);
+
+  return key;
+}
+
+
+function generateInterface (pref_name, variable, indent) {
+  console.log(pref_name, variable, indent)
+  var r = '\n' + indent + 'interface ' + pref_name + ' {\n';
+  var sub_indent = '\t' + indent;
+
+  r += Object.keys(variable).map(function(k){
+    return sub_indent + formatKey(k) + ': ' + getVariableType(variable[k], k);
+  }).join(';\n') + ';\n';
+
+  r += indent + '}\n\n';
+
+  return r;
+}
+
+
+function procClick () {
+  try {
+    var data = JSON.parse($src.value);
+    $out.value = toInterface(data);
+  } catch (err) {
+    $out.value = err;
+  }
+}
---
+++ .history/src/transform_20191211143350.ts
@@ -0,0 +1,73 @@
+
+
+var interfaceNames = ['YourInterfaceName'];
+var extra_interface = '';
+export function toInterface (variable) {
+  const data = JSON.parse(variable);
+  var result = generateInterface('YourInterfaceName', data, '');
+  return (result + '\n/* 自动生成的 Interface */\n' + extra_interface).trim();
+}
+
+function formatKey (key) {
+  // if (!/^[a-z][a-z\d]*$/i.test(key))
+  //   return JSON.stringify(key);
+
+  return key;
+}
+
+function getVariableType (variable, name) {
+  if (variable === undefined) {
+    return 'void /* undefined */';
+  }
+  
+  var type = typeof variable;
+  if (type == 'object') {
+    if (!variable)
+      return 'void /* 未知类型 */';
+    
+    if (name.slice(-2) == 'es') {
+      name = name.slice(0, -2);
+    } else if (name.slice(-1) == 's') {
+      name = name.slice(0, -1);
+    }
+    if (variable.constructor.name == 'Array') {
+      return getVariableType(variable[0], name) + '[]';
+    }
+
+    if (name) {
+      name = name[0].toUpperCase() + name.slice(1);
+    } else {
+      name = 'Unknown';
+    }
+    var ifName = interfaceNames + name;
+
+    if (interfaceNames.indexOf(ifName) != -1) {
+      var i = 1;
+      while (interfaceNames.indexOf(ifName + '_' + i) != -1) {
+        i++;
+      }
+
+      ifName = ifName + '_' + i;
+    }
+    interfaceNames.push(ifName);
+    var extra = generateInterface(ifName, variable, '');
+    extra_interface = extra + extra_interface;
+    return ifName;
+  }
+
+  return type;
+}
+
+function generateInterface (pref_name, variable, indent) {
+  console.log(pref_name, variable, indent)
+  var r = '\n' + indent + 'interface ' + pref_name + ' {\n';
+  var sub_indent = '\t' + indent;
+
+  r += Object.keys(variable).map(function(k){
+    return sub_indent + formatKey(k) + ':' + getVariableType(variable[k], k);
+  }).join(';\n') + ';\n';
+
+  r += indent + '}\n';
+
+  return r;
+}
